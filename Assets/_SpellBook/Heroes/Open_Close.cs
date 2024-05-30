28dfc05b566fa928cc077542701f9d688e463179
Mikkel <mikkelking97@gmail.com>
2024-05-30 23:27:35 +0200

    Test.

---
+++ Assets/_SpellBook/Heroes/Open_Close.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open_Close : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("WHERE IS MY DAMN INTELISENSE?!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
