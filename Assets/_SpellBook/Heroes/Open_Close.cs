f93f5867df21d5373f56bb04d99c01dc66c6bbf2
Mikkel <mikkelking97@gmail.com>
2024-05-31 20:34:22 +0200

    Font assets.

--- Assets/_SpellBook/Heroes/Open_Close.cs
+++ Assets/_SpellBook/Heroes/Open_Close.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Open_Close : MonoBehaviour
 {
+  public Button hero_image;
+  public Image hero_frame;
+
     // Start is called before the first frame update
     void Start()
     {
+      
       Debug.Log("WHERE IS MY DAMN INTELISENSE?!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+
+  public void Butonpress()
+  {
+    hero_frame.enabled = true;
+  }
+
 }
