f9ab3d22101cc3a236f4743ddaad46874ccaf4fd
Mikkel <mikkelking97@gmail.com>
2024-06-01 17:47:35 +0200

    Xp progress bar.

--- Assets/_SpellBook/Heroes/Hero_Leveling.cs
+++ Assets/_SpellBook/Heroes/Hero_Leveling.cs
@@ -7,50 +7,66 @@ using TMPro;
 
 public class Hero_Leveling : MonoBehaviour
 {
+    #region 
     public TMP_Text displaycurrentLVL;
 
-    
+
     //Your current level.
-   public int level = 1;
+    public int level = 1;
 
     //Current XP Amount.
-   public int currentXP = 0;
+    public float currentXP = 0f;
 
     //XP left until leveling up.
-   public int eXPThreshold = 10;
+    public float eXPThreshold = 10f;
 
     //How much more xp you need for each levelup.    
-   public float eXPincreasemodifier = 1.1f;
+    public float eXPincreasemodifier = 1.1f;
+    #endregion
+    
+    public Slider xpslider;
 
-   void Start()
-   {
-        displaycurrentLVL.SetText(level.ToString());
-   }
-
-//Button to click to gain XP
-public void ClickToGainXP(int e)
-{
-    currentXP += e;
-
-    if(currentXP >= eXPThreshold)
+    void Awake()
     {
-        LevelUp();
+        //xpslider = gameObject.GetComponent<Slider>();
     }
-}
+
+    void Start()
+    {
+        displaycurrentLVL.SetText(level.ToString());
+    }
+
+    //Button to click to gain XP
+    public void ClickToGainXP(int e)
+    {
+        currentXP += e;
+
+        if (currentXP >= eXPThreshold)
+        {
+            LevelUp();
+            currentXP = 0;
+        }
+    }
 
     private void LevelUp()
     {   // resets the current xp for next lvl up.
         currentXP -= eXPThreshold;
-        level ++;
-        
+        level++;
+
         /* Test one.
         float t = Mathf.Pow(eXPincreasemodifier, level);
         eXPThreshold = (int)Mathf.Floor(eXPincreasemodifier * t);
         */
-       
-       eXPThreshold = eXPThreshold + 10;
+
+        eXPThreshold = eXPThreshold + 10;
 
         displaycurrentLVL.SetText(level.ToString());
+    }
+
+    public void Update()
+    {
+        xpslider.maxValue = eXPThreshold;
+        xpslider.value = currentXP;
     }
 
 }
