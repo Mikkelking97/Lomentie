697a3d7278e72bc32eb69cdb556e4d05e7caa06e
Mikkel <mikkelking97@gmail.com>
2024-06-01 03:05:58 +0200

    Basic leveling system.

--- Assets/_SpellBook/Heroes/Hero_Leveling.cs
+++ Assets/_SpellBook/Heroes/Hero_Leveling.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Hero_Leveling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_Text displaycurrentXP;
+
+    
+    //Your current level.
+   public int level = 1;
+
+    //Current XP Amount.
+   public int currentXP = 0;
+
+    //XP left until leveling up.
+   public int eXPleft = 10;
+
+    //How much more xp you need for each levelup.    
+   public float eXPincreasemodifier = 1.5f;
+
+   void Start()
+   {
+        displaycurrentXP.SetText(level.ToString());
+   }
+
+//Button to click to gain XP
+public void ClickToGainXP(int e)
+{
+    currentXP += e;
+
+    if(currentXP >= eXPleft)
     {
-        
+        LevelUp();
+    }
+}
+
+    private void LevelUp()
+    {
+        currentXP -= eXPleft;
+        level ++;
+        float t = Mathf.Pow(eXPincreasemodifier, level);
+        Debug.Log("Reads...");
+        displaycurrentXP.SetText(level.ToString());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
